7e581340e256ff04dc211a887155f4d652aa7b5b
Justin Schreiner <jschreiner22@gmail.com>
2020-06-12 14:35:58 -0500

    created basic festival controller

---
+++ app/controllers/festival_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class FestivalController < ApplicationController
+  def list
+    @festivals = Festival.all
+  end
+
+  def show
+    @festival = Festival.find(params[:id])
+  end
+
+  def new
+    @festival = Festival.new
+  end
+
+  def festival_params
+    params.require(:festivals).permit(:title, :price, :subject_id, :description)
+  end
+
+  def create
+    @festival = Festival.new(festival_params)
+
+    if @festival.save
+      redirect_to action: 'list'
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @festival = Festival.find(params[:id])
+  end
+
+  def festival_param
+    params.require(:festival).permit(:title, :price, :subject_id, :description)
+  end
+
+  def update
+    @festival = Festival.find(params[:id])
+
+    if @festival.update_attributes(festival_param)
+      redirect_to action: 'show', id: @festival
+    else
+      render action: 'edit'
+    end
+  end
+
+  def delete
+    Festival.find(params[:id]).destroy
+    redirect_to action: 'list'
+  end
+end
