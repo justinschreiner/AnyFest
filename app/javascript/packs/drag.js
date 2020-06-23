d60ab18157b208b342e17aa4273fdb1802d0bb42
Justin Schreiner <jschreiner22@gmail.com>
2020-06-23 17:45:59 -0500

    interactjs dropzones working

--- app/javascript/packs/drag.js
+++ app/javascript/packs/drag.js
@@ -46,7 +46,7 @@ interact(".drag-drop")
     modifiers: [
       // keep the edges inside the parent
       interact.modifiers.restrictEdges({
-        outer: "parent",
+        // outer: "parent",
       }),
 
       // minimum size
@@ -63,8 +63,41 @@ interact(".drag-drop")
     onmove: dragMoveListener,
     modifiers: [
       interact.modifiers.restrictRect({
-        restriction: "parent",
+        // restriction: "parent",
         endOnly: true,
       }),
     ],
   });
+
+interact(".dropzone").dropzone({
+  // only accept elements matching this CSS selector
+  accept: ".drag-drop",
+  // Require a 75% element overlap for a drop to be possible
+  overlap: 0.75,
+
+  // listen for drop related events:
+
+  ondropactivate: function (event) {
+    // add active dropzone feedback
+    event.target.classList.add("drop-active");
+  },
+  ondragenter: function (event) {
+    var draggableElement = event.relatedTarget;
+    var dropzoneElement = event.target;
+
+    // feedback the possibility of a drop
+    dropzoneElement.classList.add("drop-target");
+    draggableElement.classList.add("can-drop");
+  },
+  ondragleave: function (event) {
+    // remove the drop feedback style
+    event.target.classList.remove("drop-target");
+    event.relatedTarget.classList.remove("can-drop");
+  },
+  ondrop: function (event) {},
+  ondropdeactivate: function (event) {
+    // remove active dropzone feedback
+    event.target.classList.remove("drop-active");
+    event.target.classList.remove("drop-target");
+  },
+});
