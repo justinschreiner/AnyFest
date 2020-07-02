36a147a6927e856a03db930cb4dc8eeb3b6d0f6d
Justin Schreiner <jschreiner22@gmail.com>
2020-07-02 10:35:13 -0500

    getting rid of color functions for now

--- app/javascript/packs/drag.js
+++ app/javascript/packs/drag.js
@@ -1,13 +1,6 @@
 import interact from "interactjs";
 
 window.addEventListener("DOMContentLoaded", (event) => {
-  const primaryColor = getAverageRGB(
-    document.getElementsByTagName("img")[0]
-  )[0];
-  const secondaryColor = getAverageRGB(
-    document.getElementsByTagName("img")[0]
-  )[1];
-
   // Allows boxes to be dragged
   window.dragMoveListener = function (event) {
     var target, x, y;
@@ -22,8 +15,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     return target.setAttribute("data-y", y);
   };
 
-  // Allows boxes to be resized
-
+  // Allows boxes to be resized and dragged
   interact(".drag-drop")
     .resizable({
       edges: { left: true, right: true, bottom: true, top: true },
@@ -77,7 +69,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
 
   // Creates new sections or days by dragging out of the sidebar
-
   interact(".item")
     .draggable({
       inertia: true,
@@ -123,7 +114,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
         } else {
           element.classList.add("section-drag");
         }
-        element.style.border = "2px solid" + primaryColor;
 
         // Add the cloned object to the document
         const container = document.querySelector(".container");
@@ -147,7 +137,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
       interaction.start({ name: "drag" }, event.interactable, element);
     });
 
-  // Template image dropzone
+  // Template image dropzone, accepts days and sections
   interact(".dropzone").dropzone({
     accept: ".drag-drop",
     overlap: 0.75,
@@ -175,7 +165,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     ondropdeactivate: function (event) {},
   });
 
-  // Day dropzone for sections
+  // Day dropzone, only accepts sections
   interact("#day").dropzone({
     accept: ".section-drag",
     overlap: 0.75,
@@ -190,12 +180,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
     },
     ondragenter: function (event) {
       // feedback the possibility of a drop
-      event.target.style.border = "2px solid " + secondaryColor;
+      event.target.style.border = "2px solid #fff";
       event.relatedTarget.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
     },
     ondragleave: function (event) {
       // remove the drop feedback style
-      event.target.style.border = "2px solid " + primaryColor;
+      event.target.style.border = "2px solid #000";
       event.relatedTarget.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
     },
     ondrop: function (event) {
@@ -215,12 +205,12 @@ window.addEventListener("DOMContentLoaded", (event) => {
     },
     ondropdeactivate: function (event) {
       // remove active dropzone feedback
-      event.target.style.border = "2px solid " + primaryColor;
+      event.target.style.border = "2px solid #000";
       event.relatedTarget.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
     },
   });
 
-  // Trashcan dropzone
+  // Trashcan dropzone, accepts days and sections
   interact("#trash").dropzone({
     accept: ".drag-drop",
     overlap: 0.1,
@@ -427,6 +417,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
   };
 
+  // Keeps track of whether or not the user is clicking
+  // Used to see if the user has dropped something in the trash
   var mouseDown = 0;
   window.onmousedown = function () {
     ++mouseDown;
@@ -434,99 +426,4 @@ window.addEventListener("DOMContentLoaded", (event) => {
   window.onmouseup = function () {
     --mouseDown;
   };
-
-  function getAverageRGB(imgEl) {
-    var blockSize = 5, // only visit every 5 pixels
-      defaultRGB = { r: 0, g: 0, b: 0 }, // for non-supporting envs
-      canvas = document.createElement("canvas"),
-      context = canvas.getContext && canvas.getContext("2d"),
-      data,
-      width,
-      height,
-      i = -4,
-      length,
-      rgb = { r: 0, g: 0, b: 0 },
-      count = 0;
-
-    if (!context) {
-      return defaultRGB;
-    }
-
-    height = canvas.height =
-      imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-    width = canvas.width =
-      imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-
-    context.drawImage(imgEl, 0, 0);
-
-    try {
-      data = context.getImageData(0, 0, width, height);
-    } catch (e) {
-      /* security error, img on diff domain */
-      return defaultRGB;
-    }
-
-    length = data.data.length;
-
-    while ((i += blockSize * 4) < length) {
-      ++count;
-      rgb.r += data.data[i];
-      rgb.g += data.data[i + 1];
-      rgb.b += data.data[i + 2];
-    }
-
-    // ~~ used to floor values
-    rgb.r = ~~(rgb.r / count);
-    rgb.g = ~~(rgb.g / count);
-    rgb.b = ~~(rgb.b / count);
-
-    var rgbString =
-      rgb.r.toString() + "," + rgb.g.toString() + "," + rgb.b.toString();
-
-    return lightOrDark(rgbString);
-  }
-
-  function lightOrDark(color) {
-    // Variables for red, green, blue values
-    var r, g, b, hsp;
-
-    // Check the format of the color, HEX or RGB?
-    if (color.match(/^rgb/)) {
-      // If RGB --> store the red, green, blue values in separate variables
-      color = color.match(
-        /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/
-      );
-
-      r = color[1];
-      g = color[2];
-      b = color[3];
-    } else {
-      // If hex --> Convert it to RGB: http://gist.github.com/983661
-      color = +(
-        "0x" + color.slice(1).replace(color.length < 5 && /./g, "$&$&")
-      );
-
-      r = color >> 16;
-      g = (color >> 8) & 255;
-      b = color & 255;
-    }
-
-    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
-    hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
-
-    // Using the HSP value, determine whether the color is light or dark
-    if (hsp > 127.5) {
-      return generateColors(0); // the color is light
-    } else {
-      return generateColors(1); // the color is dark
-    }
-  }
-
-  function generateColors(b) {
-    if (b == 0) {
-      return ["#000", "#fff"];
-    } else {
-      return ["#fff", "#000"];
-    }
-  }
 });
