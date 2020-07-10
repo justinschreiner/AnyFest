72ee97cca5dbb9f36c8477e2838cd4e4253444ae
Justin Schreiner <jschreiner22@gmail.com>
2020-07-10 12:33:13 -0500

    collapsible accordians working for each instance in sections setting form

--- app/javascript/packs/makeSection.js
+++ app/javascript/packs/makeSection.js
@@ -87,6 +87,73 @@ window.makeDay = function (dayHeight, dayWidth, dayXOffset, dayYOffset) {
   });
 };
 
+window.makeDayIdUnique = function () {
+  window.addEventListener("load", (event) => {
+    var index = Date.now() + Math.round(Math.random() * 100000);
+    var accordionContainer = document.getElementById("accordionDay");
+    accordionContainer.setAttribute("id", "accordionDay" + index.toString());
+
+    var dayHeading = document.getElementById("dayHeading");
+    dayHeading.setAttribute("id", "dayHeading" + index.toString());
+
+    var headerContent = document.getElementById("dayAccordionHeader");
+    headerContent.setAttribute("id", "dayAccordionHeader" + index.toString());
+    headerContent.setAttribute(
+      "aria-controls",
+      "collapseDay" + index.toString()
+    );
+    headerContent.setAttribute("href", "#collapseDay" + index.toString());
+
+    var content = document.getElementById("collapseDay");
+    content.setAttribute("id", "collapseDay" + index.toString());
+    content.setAttribute("aria-labelledby", "dayHeading" + index.toString());
+    content.setAttribute("data-parent", "#accordionDay" + index.toString());
+
+    var button = document.getElementById("innerDayCollapse");
+    button.setAttribute("aria-controls", "collapseDay" + index.toString());
+    button.setAttribute("href", "#collapseDay" + index.toString());
+    button.setAttribute("id", "innerDayCollapse" + index.toString());
+  });
+};
+
+window.makeSectionIdUnique = function () {
+  window.addEventListener("load", (event) => {
+    var index = Date.now() + Math.round(Math.random() * 100000);
+    var accordionContainer = document.getElementById("accordionSection");
+    accordionContainer.setAttribute(
+      "id",
+      "accordionSection" + index.toString()
+    );
+
+    var sectionHeading = document.getElementById("sectionHeading");
+    sectionHeading.setAttribute("id", "sectionHeading" + index.toString());
+
+    var headerContent = document.getElementById("sectionAccordionHeader");
+    headerContent.setAttribute(
+      "id",
+      "sectionAccordionHeader" + index.toString()
+    );
+    headerContent.setAttribute(
+      "aria-controls",
+      "collapseSection" + index.toString()
+    );
+    headerContent.setAttribute("href", "#collapseSection" + index.toString());
+
+    var content = document.getElementById("collapseSection");
+    content.setAttribute("id", "collapseSection" + index.toString());
+    content.setAttribute(
+      "aria-labelledby",
+      "sectionHeading" + index.toString()
+    );
+    content.setAttribute("data-parent", "#accordionSection" + index.toString());
+
+    var button = document.getElementById("innerSectionCollapse");
+    button.setAttribute("aria-controls", "collapseSection" + index.toString());
+    button.setAttribute("href", "#collapseSection" + index.toString());
+    button.setAttribute("id", "innerSectionCollapse" + index.toString());
+  });
+};
+
 window.resetIndex = function () {
   index = 0;
 };
