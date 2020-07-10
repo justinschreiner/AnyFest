9e46ba0a9f72c8cac322ec8c85063f7f1952d5bc
Justin Schreiner <jschreiner22@gmail.com>
2020-07-10 09:24:24 -0500

    adding validations for section attributes in template form

--- app/models/template.rb
+++ app/models/template.rb
@@ -3,10 +3,11 @@ class Template < ApplicationRecord
     belongs_to :user
     has_many :days
     has_one_attached :background_image
-    accepts_nested_attributes_for :days
+    accepts_nested_attributes_for :days, allow_destroy: true
 
     validates :festival_id, :name, :background_image, presence: true, if: :active_or_create?
-    validates :days,                                  presence: true, if: :position_or_create?
+    validates :days,                                  presence: true, if: :active_or_position?
+    validate :nested_validations,                                     if: :active_or_settings?
 
     def active?
         status == 'active'
@@ -16,11 +17,24 @@ class Template < ApplicationRecord
         (status == "create_template") || active?
     end
 
-    def position_or_create?
+    def active_or_position?
         (status == "position_sections") || active?
     end
 
-    def settings_or_create?
+    def active_or_settings?
         (status == "section_settings") || active?
+    end
+
+    def nested_validations
+        self.days.each do |day|
+            day.sections.each do |section|
+                unless section.name.present?
+                    self.errors.add(:base, "Section name field can't be blank.")
+                end
+                unless section.max_act_count.present?
+                    self.errors.add(:base, "Maximum acts field can't be blank.")
+                end
+            end
+        end
     end
 end
