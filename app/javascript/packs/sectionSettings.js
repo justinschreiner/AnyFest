42494bad73a763ebf671f8a0d6ae203530ae1319
Justin Schreiner <jschreiner22@gmail.com>
2020-07-13 11:02:07 -0500

    sections can accept multiple colors, adding documentation to javascript

--- app/javascript/packs/sectionSettings.js
+++ app/javascript/packs/sectionSettings.js
@@ -1,5 +1,9 @@
+// Javascript to help the section settings page
+
 var index = 0;
 
+// Creates the boxes on the preview images to indicate which section is being edited
+// Creates a green section inside a gray day
 window.makeBoxes = function (
   dayHeight,
   dayWidth,
@@ -14,12 +18,14 @@ window.makeBoxes = function (
     var image = document.getElementsByClassName("preview_image")[0];
     var container = document.getElementsByClassName("container")[index];
 
+    // Make the divs that will be the boxes
     var day = document.createElement("div");
     var section = document.createElement("div");
 
     day.setAttribute("class", "surrounding-day");
     section.setAttribute("class", "section");
 
+    // Make the boxes the right size based on their size in the db
     day.setAttribute(
       "style",
       "height: " +
@@ -44,26 +50,33 @@ window.makeBoxes = function (
         ((sectionXOffset * 100.0) / 10000.0).toString() +
         "%; z-index: 2;"
     );
+
+    // Setting the size of the form container
     container.setAttribute(
       "style",
       "height: " + image.height.toString() + "px;" //workaround for now, probably a more efficient way to do this
     );
 
+    // Append the new boxes to the image
     container.appendChild(day);
     container.appendChild(section);
     index++;
   });
 };
 
+// Creates the boxes on the preview images to indicate which day is being edited
+// Creates a green day with no sections
 window.makeDay = function (dayHeight, dayWidth, dayXOffset, dayYOffset) {
   window.addEventListener("load", (event) => {
     var image = document.getElementsByClassName("preview_image")[0];
     var container = document.getElementsByClassName("container")[index];
 
+    // Create the div that will represent the day
     var day = document.createElement("div");
 
     day.setAttribute("class", "solo-day");
 
+    // Make the day box the right size based off of the sizes in the db
     day.setAttribute(
       "style",
       "height: " +
@@ -77,19 +90,26 @@ window.makeDay = function (dayHeight, dayWidth, dayXOffset, dayYOffset) {
         "%; z-index: 1;"
     );
 
+    // Adjust the container for the form
     container.setAttribute(
       "style",
       "height: " + image.height.toString() + "px;" //workaround for now, probably a more efficient way to do this
     );
 
+    // Append the day box to the image
     container.appendChild(day);
     index++;
   });
 };
 
+// Make the accordion actions reference unique day values
+// Ensures the right form section is expanding/collapsing
 window.makeDayIdUnique = function () {
   window.addEventListener("load", (event) => {
+    // Create a unique index for this day
     var index = Date.now() + Math.round(Math.random() * 100000);
+
+    // Add this index to identifying attributes of this accordion
     var accordionContainer = document.getElementById("accordionDay");
     accordionContainer.setAttribute("id", "accordionDay" + index.toString());
 
@@ -116,9 +136,14 @@ window.makeDayIdUnique = function () {
   });
 };
 
+// Make the accordion actions reference unique day values
+// Ensures the right form section is expanding/collapsing
 window.makeSectionIdUnique = function () {
   window.addEventListener("load", (event) => {
+    // Create a unique index for this section
     var index = Date.now() + Math.round(Math.random() * 100000);
+
+    // Add this index to identifying attributes of this accordion
     var accordionContainer = document.getElementById("accordionSection");
     accordionContainer.setAttribute(
       "id",
@@ -152,6 +177,7 @@ window.makeSectionIdUnique = function () {
     button.setAttribute("href", "#collapseSection" + index.toString());
     button.setAttribute("id", "innerSectionCollapse" + index.toString());
 
+    // Give the 'add color' button inside of this accordion reference its own unique index
     var checkBox = document.getElementById("alternating-color");
     checkBox.setAttribute("data-id", (index + 5).toString());
 
@@ -160,10 +186,23 @@ window.makeSectionIdUnique = function () {
   });
 };
 
+// Add '[]' to the end of the 'name' attribute for text color fields
+// This allows rails to accept multiple inputs for color fields
+window.prepColorFields = function () {
+  var colorFieldCount = document.getElementsByClassName("text-color").length;
+  for (i = 0; i < colorFieldCount; i++) {
+    var tempColorField = document.getElementsByClassName("text-color")[i];
+    var currentName = tempColorField.getAttribute("name");
+    tempColorField.setAttribute("name", currentName + "[]");
+  }
+};
+
+// Resets the global index variable
 window.resetIndex = function () {
   index = 0;
 };
 
+// Expands and collapses the add color icon based on whether or not the alternating colors box is checked or not
 window.collapseAddColor = function (self) {
   var id = self.getAttribute("data-id");
   var content = document.getElementById(id);
--- db/migrate/20200709143345_change_text_color_type.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTextColorType < ActiveRecord::Migration[6.0]
-  def change
-    change_column(:sections, :text_colors, :string)
-  end
-end
