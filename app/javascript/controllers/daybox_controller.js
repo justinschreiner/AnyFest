a823cf293f74bbc69abeca8b4580627d3ff68fc1
Justin Schreiner <jschreiner22@gmail.com>
2020-07-14 12:45:36 -0500

    refreshing works with stimulus

--- app/javascript/controllers/daybox_controller.js
+++ app/javascript/controllers/daybox_controller.js
@@ -4,71 +4,84 @@ export default class extends Controller {
   static targets = ["output"];
 
   connect() {
-    // Make Box
-    // Get data from view
-    var dayHeight = this.data.get("height");
-    var dayWidth = this.data.get("width");
-    var dayYOffset = this.data.get("y-offset");
-    var dayXOffset = this.data.get("x-offset");
+    this.element
+      .getElementsByTagName("img")[0]
+      .addEventListener("load", (e) => {
+        // Make Box
+        // Get data from view
+        var dayHeight = this.data.get("height");
+        var dayWidth = this.data.get("width");
+        var dayYOffset = this.data.get("y-offset");
+        var dayXOffset = this.data.get("x-offset");
 
-    var image = document.getElementsByClassName("preview_image")[0];
-    var container = this.outputTarget;
+        var image = document.getElementsByClassName("preview_image")[0];
+        var container = this.outputTarget;
 
-    // Create the div that will represent the day
-    var day = document.createElement("div");
+        // Create the div that will represent the day
+        var day = document.createElement("div");
 
-    day.setAttribute("class", "solo-day");
+        day.setAttribute("class", "solo-day");
 
-    // Make the day box the right size based off of the sizes in the db
-    day.setAttribute(
-      "style",
-      "height: " +
-        ((dayHeight * image.height) / 10000.0).toString() +
-        "px; width: " +
-        ((dayWidth * 100.0) / 10000.0).toString() +
-        "%; margin-top: " +
-        ((dayYOffset * image.height) / 10000.0).toString() +
-        "px; margin-left: " +
-        ((dayXOffset * 100.0) / 10000.0).toString() +
-        "%; z-index: 1;"
-    );
+        // Make the day box the right size based off of the sizes in the db
+        day.setAttribute(
+          "style",
+          "height: " +
+            ((dayHeight * image.height) / 10000.0).toString() +
+            "px; width: " +
+            ((dayWidth * 100.0) / 10000.0).toString() +
+            "%; margin-top: " +
+            ((dayYOffset * image.height) / 10000.0).toString() +
+            "px; margin-left: " +
+            ((dayXOffset * 100.0) / 10000.0).toString() +
+            "%; z-index: 1;"
+        );
 
-    // Adjust the container for the form
-    container.setAttribute(
-      "style",
-      "height: " + image.height.toString() + "px;" //workaround for now, probably a more efficient way to do this
-    );
+        // Adjust the container for the form
+        container.setAttribute(
+          "style",
+          "height: " + image.height.toString() + "px;" //workaround for now, probably a more efficient way to do this
+        );
 
-    // Append the day box to the image
-    container.appendChild(day);
+        // Append the day box to the image
+        container.appendChild(day);
 
-    // Create a unique index for this day
-    var index = Date.now() + Math.round(Math.random() * 100000);
+        // Create a unique index for this day
+        var index = Date.now() + Math.round(Math.random() * 100000);
 
-    // Make Id Unique for accordion
-    // Add this index to identifying attributes of this accordion
-    var accordionContainer = document.getElementById("accordionDay");
-    accordionContainer.setAttribute("id", "accordionDay" + index.toString());
+        // Make Id Unique for accordion
+        // Add this index to identifying attributes of this accordion
+        var accordionContainer = document.getElementById("accordionDay");
+        accordionContainer.setAttribute(
+          "id",
+          "accordionDay" + index.toString()
+        );
 
-    var dayHeading = document.getElementById("dayHeading");
-    dayHeading.setAttribute("id", "dayHeading" + index.toString());
+        var dayHeading = document.getElementById("dayHeading");
+        dayHeading.setAttribute("id", "dayHeading" + index.toString());
 
-    var headerContent = document.getElementById("dayAccordionHeader");
-    headerContent.setAttribute("id", "dayAccordionHeader" + index.toString());
-    headerContent.setAttribute(
-      "aria-controls",
-      "collapseDay" + index.toString()
-    );
-    headerContent.setAttribute("href", "#collapseDay" + index.toString());
+        var headerContent = document.getElementById("dayAccordionHeader");
+        headerContent.setAttribute(
+          "id",
+          "dayAccordionHeader" + index.toString()
+        );
+        headerContent.setAttribute(
+          "aria-controls",
+          "collapseDay" + index.toString()
+        );
+        headerContent.setAttribute("href", "#collapseDay" + index.toString());
 
-    var content = document.getElementById("collapseDay");
-    content.setAttribute("id", "collapseDay" + index.toString());
-    content.setAttribute("aria-labelledby", "dayHeading" + index.toString());
-    content.setAttribute("data-parent", "#accordionDay" + index.toString());
+        var content = document.getElementById("collapseDay");
+        content.setAttribute("id", "collapseDay" + index.toString());
+        content.setAttribute(
+          "aria-labelledby",
+          "dayHeading" + index.toString()
+        );
+        content.setAttribute("data-parent", "#accordionDay" + index.toString());
 
-    var button = document.getElementById("innerDayCollapse");
-    button.setAttribute("aria-controls", "collapseDay" + index.toString());
-    button.setAttribute("href", "#collapseDay" + index.toString());
-    button.setAttribute("id", "innerDayCollapse" + index.toString());
+        var button = document.getElementById("innerDayCollapse");
+        button.setAttribute("aria-controls", "collapseDay" + index.toString());
+        button.setAttribute("href", "#collapseDay" + index.toString());
+        button.setAttribute("id", "innerDayCollapse" + index.toString());
+      });
   }
 }
--- app/javascript/controllers/sectionbox_controller.js
+++ app/javascript/controllers/sectionbox_controller.js
@@ -4,121 +4,137 @@ export default class extends Controller {
   static targets = ["output"];
 
   connect() {
-    // Make Boxes
-    // Get data from view
-    var dayHeight = this.data.get("dayheight");
-    var dayWidth = this.data.get("daywidth");
-    var dayYOffset = this.data.get("day-y-offset");
-    var dayXOffset = this.data.get("day-x-offset");
+    this.element
+      .getElementsByTagName("img")[0]
+      .addEventListener("load", (e) => {
+        // Make Boxes
+        // Get data from view
+        var dayHeight = this.data.get("dayheight");
+        var dayWidth = this.data.get("daywidth");
+        var dayYOffset = this.data.get("day-y-offset");
+        var dayXOffset = this.data.get("day-x-offset");
 
-    var sectionHeight = this.data.get("sectionheight");
-    var sectionWidth = this.data.get("sectionwidth");
-    var sectionYOffset = this.data.get("section-y-offset");
-    var sectionXOffset = this.data.get("section-x-offset");
+        var sectionHeight = this.data.get("sectionheight");
+        var sectionWidth = this.data.get("sectionwidth");
+        var sectionYOffset = this.data.get("section-y-offset");
+        var sectionXOffset = this.data.get("section-x-offset");
 
-    var image = document.getElementsByClassName("preview_image")[0];
-    var container = this.outputTarget;
+        var image = document.getElementsByClassName("preview_image")[0];
+        var container = this.outputTarget;
 
-    // Make the divs that will be the boxes
-    var day = document.createElement("div");
-    var section = document.createElement("div");
+        // Make the divs that will be the boxes
+        var day = document.createElement("div");
+        var section = document.createElement("div");
 
-    day.setAttribute("class", "surrounding-day");
-    section.setAttribute("class", "section");
+        day.setAttribute("class", "surrounding-day");
+        section.setAttribute("class", "section");
 
-    // Make the boxes the right size based on their size in the db
-    day.setAttribute(
-      "style",
-      "height: " +
-        ((dayHeight * image.height) / 10000.0).toString() +
-        "px; width: " +
-        ((dayWidth * 100.0) / 10000.0).toString() +
-        "%; margin-top: " +
-        ((dayYOffset * image.height) / 10000.0).toString() +
-        "px; margin-left: " +
-        ((dayXOffset * 100.0) / 10000.0).toString() +
-        "%; z-index: 1;"
-    );
-    section.setAttribute(
-      "style",
-      "height: " +
-        ((sectionHeight * image.height) / 10000.0).toString() +
-        "px; width: " +
-        ((sectionWidth * 100.0) / 10000.0).toString() +
-        "%; margin-top: " +
-        ((sectionYOffset * image.height) / 10000.0).toString() +
-        "px; margin-left: " +
-        ((sectionXOffset * 100.0) / 10000.0).toString() +
-        "%; z-index: 2;"
-    );
+        // Make the boxes the right size based on their size in the db
+        day.setAttribute(
+          "style",
+          "height: " +
+            ((dayHeight * image.height) / 10000.0).toString() +
+            "px; width: " +
+            ((dayWidth * 100.0) / 10000.0).toString() +
+            "%; margin-top: " +
+            ((dayYOffset * image.height) / 10000.0).toString() +
+            "px; margin-left: " +
+            ((dayXOffset * 100.0) / 10000.0).toString() +
+            "%; z-index: 1;"
+        );
+        section.setAttribute(
+          "style",
+          "height: " +
+            ((sectionHeight * image.height) / 10000.0).toString() +
+            "px; width: " +
+            ((sectionWidth * 100.0) / 10000.0).toString() +
+            "%; margin-top: " +
+            ((sectionYOffset * image.height) / 10000.0).toString() +
+            "px; margin-left: " +
+            ((sectionXOffset * 100.0) / 10000.0).toString() +
+            "%; z-index: 2;"
+        );
 
-    // Setting the size of the form container
-    container.setAttribute(
-      "style",
-      "height: " + image.height.toString() + "px;" //workaround for now, probably a more efficient way to do this
-    );
+        // Setting the size of the form container
+        container.setAttribute(
+          "style",
+          "height: " + image.height.toString() + "px;" //workaround for now, probably a more efficient way to do this
+        );
 
-    // Append the new boxes to the image
-    container.appendChild(day);
-    container.appendChild(section);
+        // Append the new boxes to the image
+        container.appendChild(day);
+        container.appendChild(section);
 
-    // Make Unique Id's for accordions
-    // Create a unique index for this section
-    var index = Date.now() + Math.round(Math.random() * 100000);
+        // Make Unique Id's for accordions
+        // Create a unique index for this section
+        var index = Date.now() + Math.round(Math.random() * 100000);
 
-    // Add this index to identifying attributes of this accordion
-    var accordionContainer = document.getElementById("accordionSection");
-    accordionContainer.setAttribute(
-      "id",
-      "accordionSection" + index.toString()
-    );
+        // Add this index to identifying attributes of this accordion
+        var accordionContainer = document.getElementById("accordionSection");
+        accordionContainer.setAttribute(
+          "id",
+          "accordionSection" + index.toString()
+        );
 
-    var sectionHeading = document.getElementById("sectionHeading");
-    sectionHeading.setAttribute("id", "sectionHeading" + index.toString());
+        var sectionHeading = document.getElementById("sectionHeading");
+        sectionHeading.setAttribute("id", "sectionHeading" + index.toString());
 
-    var headerContent = document.getElementById("sectionAccordionHeader");
-    headerContent.setAttribute(
-      "id",
-      "sectionAccordionHeader" + index.toString()
-    );
-    headerContent.setAttribute(
-      "aria-controls",
-      "collapseSection" + index.toString()
-    );
-    headerContent.setAttribute("href", "#collapseSection" + index.toString());
+        var headerContent = document.getElementById("sectionAccordionHeader");
+        headerContent.setAttribute(
+          "id",
+          "sectionAccordionHeader" + index.toString()
+        );
+        headerContent.setAttribute(
+          "aria-controls",
+          "collapseSection" + index.toString()
+        );
+        headerContent.setAttribute(
+          "href",
+          "#collapseSection" + index.toString()
+        );
 
-    var content = document.getElementById("collapseSection");
-    content.setAttribute("id", "collapseSection" + index.toString());
-    content.setAttribute(
-      "aria-labelledby",
-      "sectionHeading" + index.toString()
-    );
-    content.setAttribute("data-parent", "#accordionSection" + index.toString());
+        var content = document.getElementById("collapseSection");
+        content.setAttribute("id", "collapseSection" + index.toString());
+        content.setAttribute(
+          "aria-labelledby",
+          "sectionHeading" + index.toString()
+        );
+        content.setAttribute(
+          "data-parent",
+          "#accordionSection" + index.toString()
+        );
 
-    var button = document.getElementById("innerSectionCollapse");
-    button.setAttribute("aria-controls", "collapseSection" + index.toString());
-    button.setAttribute("href", "#collapseSection" + index.toString());
-    button.setAttribute("id", "innerSectionCollapse" + index.toString());
+        var button = document.getElementById("innerSectionCollapse");
+        button.setAttribute(
+          "aria-controls",
+          "collapseSection" + index.toString()
+        );
+        button.setAttribute("href", "#collapseSection" + index.toString());
+        button.setAttribute("id", "innerSectionCollapse" + index.toString());
 
-    // Give the 'add color' button inside of this accordion reference its own unique index
-    var checkBox = document.getElementById("alternating-color");
-    checkBox.setAttribute("data-id", (index + 5).toString());
+        // Give the 'add color' button inside of this accordion reference its own unique index
+        var checkBox = document.getElementById("alternating-color");
+        checkBox.setAttribute("data-id", (index + 5).toString());
 
-    var colorContent = document.getElementById("colors");
-    colorContent.setAttribute("id", (index + 5).toString());
+        var colorContent = document.getElementById("colors");
+        colorContent.setAttribute("id", (index + 5).toString());
 
-    // Add '[]' to the end of the 'name' attribute for text color fields
-    // This allows rails to accept multiple inputs for color fields
-    var colorFieldCount = document.getElementsByClassName("text-color").length;
-    for (var i = 0; i < colorFieldCount; i++) {
-      var tempColorField = document.getElementsByClassName("text-color")[i];
-      var currentName = tempColorField.getAttribute("name");
-      if (
-        currentName.substr(currentName.length - 2, currentName.length - 1) !=
-        "[]"
-      ) {
-        tempColorField.setAttribute("name", currentName + "[]");
-      }
-    }
+        // Add '[]' to the end of the 'name' attribute for text color fields
+        // This allows rails to accept multiple inputs for color fields
+        var colorFieldCount = document.getElementsByClassName("text-color")
+          .length;
+        for (var i = 0; i < colorFieldCount; i++) {
+          var tempColorField = document.getElementsByClassName("text-color")[i];
+          var currentName = tempColorField.getAttribute("name");
+          if (
+            currentName.substr(
+              currentName.length - 2,
+              currentName.length - 1
+            ) != "[]"
+          ) {
+            tempColorField.setAttribute("name", currentName + "[]");
+          }
+        }
+      });
   }
 }
