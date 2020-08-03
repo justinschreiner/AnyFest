f5c308952219d5e1421bb775fb7a502d1f522904
Justin Schreiner <jschreiner22@gmail.com>
2020-08-03 10:38:44 -0500

    docker changes and changing lineup div resizing to run through stimulus controller

---
+++ app/javascript/controllers/download_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "stimulus";
+import html2canvas from "html2canvas";
+
+export default class extends Controller {
+  connect() {
+    // Wait for lineup image to completely render
+    document
+      .getElementsByClassName("image")[0]
+      .addEventListener("load", (e) => {
+        var img = document.getElementById("lineup");
+        var link = document.getElementById("download-link");
+        html2canvas(img).then(function (canvas) {
+          var imgFile = canvas.toDataURL("image/png");
+          link.href = imgFile;
+        });
+      });
+  }
+}
--- app/javascript/lineup.js
+++ app/javascript/lineup.js
@@ -1,10 +1,10 @@
-import html2canvas from "html2canvas";
+// import html2canvas from "html2canvas";
 
-window.prepDownloadLink = function () {
-  var img = document.getElementById("lineup");
-  var link = document.getElementById("download-link");
-  html2canvas(img).then(function (canvas) {
-    var imgFile = canvas.toDataURL("image/png");
-    link.href = imgFile;
-  });
-};
+// window.prepDownloadLink = function () {
+//   var img = document.getElementById("lineup");
+//   var link = document.getElementById("download-link");
+//   html2canvas(img).then(function (canvas) {
+//     var imgFile = canvas.toDataURL("image/png");
+//     link.href = imgFile;
+//   });
+// };
