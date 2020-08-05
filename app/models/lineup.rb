1348e3c9c2db39196dd48a9d63ac58993a6546b9
Justin Schreiner <jschreiner22@gmail.com>
2020-08-05 13:14:36 -0500

    reorganizing model files

--- app/models/lineup.rb
+++ app/models/lineup.rb
@@ -1,10 +1,10 @@
 class Lineup < ApplicationRecord
+    include PgSearch::Model
+    pg_search_scope :search_by_name, against: :name
+
     belongs_to :template
     belongs_to :user
     has_many :section_acts
 
     accepts_nested_attributes_for :section_acts
-
-    include PgSearch::Model
-    pg_search_scope :search_by_name, against: :name
 end
